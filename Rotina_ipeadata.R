6bb11eed1e0a5050521d55ed1d90dc31371c9301
JoseliFerraz <joseli.ferraz@ipea.gov.br>
2018-08-18 18:03:09 -0300

    Update Rotina_ipeadata.R

--- Rotina_ipeadata.R
+++ Rotina_ipeadata.R
@@ -1,9 +1,22 @@
-# -----------------------------------------------
-# Programacao rotina de verificacao
-# Incio 14/8/2018
-# 
+
+# --------------------------------------------------------- #
+# |   INSTITUTO DE PESQUISA ECONOMICA APLICADA - IPEA     | #
+# |                    PROJETO IPEADATA                   | #
+# --------------------------------------------------------- #
+# |   COORDENADOR:                    | #
+# --------------------------------------------------------- #
+# |   PROGRAMADORA: Joseli Moreira Ferraz                 | #
+# |   PROGRAMADORA: Gabriella do Nascimento Pinheiro      | #
+# --------------------------------------------------------- #
+# |   Rotina de verificacao                               | #
+# |   Incio 14/8/2018                                     | #
+# --------------------------------------------------------- #
+
 # -----------------------------------------------
 
+# Construcao do algoritmo para verificacao delimitando 
+# a regiao Brasil
+# -----------------------------------------------
 
 # -----------------------------------------------
 # Pacotes  
@@ -12,6 +25,7 @@
 #install.packages("knitr") #pacote para bordas de tabela 
 #install.packages("gridExtra")
 library(ipeaData)
+
 library(data.table)
 library(knitr)
 library(gridExtra)
@@ -19,23 +33,39 @@ library(gridExtra)
 options(digits=17)
 # -----------------------------------------------
 
-# Funciona
-data <- ipeadata('BPC_defi')
+# Funciona BPC_defi
+
+# -----------------------------------------------
+# Pra aAnalise Regional social
+# -----------------------------------------------
+data <- ipeadata('DISOC_PPY')
 
 # Selecao por: Brasil
+#data_brasil=data[data$TERCODIGO==0]
 data_brasil=data[data$TERCODIGO==0]
+
+
+
+# -----------------------------------------------
+# Pra aAnalise Macroeconomica
+# -----------------------------------------------
+data_brasil=data[data$TERNOME=="Brasil"]
+data_brasil=data
+
 
 # -----------------------------------------------
 # Analise de consistencia : Padrao da Planilha
 # -----------------------------------------------
 
 # Calculo dos desvios
-media=mean(data_brasil$VALVALOR)
-dp1=sd(data_brasil$VALVALOR)
+media=mean(data_brasil$VALVALOR,na.rm=T)
+dp1=sd(data_brasil$VALVALOR,na.rm=T)
 dp2=2*dp1
 dp3=3*dp1
 dp4=4*dp1
 dp5=5*dp1
+
+# Gerando tabela 
 # kable(table(media,dp1,dp2,dp3,dp4,dp5))
 grid.table(c(media,dp1,dp2,dp3,dp4,dp5),c("media","+1DP", "+2DP", "+3DP","+4DP","+5DP"))
 
@@ -55,32 +85,117 @@ dp_mais3=media+dp3
 dp_mais4=media+dp4
 dp_mais5=media+dp5
 
-
-
 # -----------------------------------------------------
 # Tabela Calculos consistencia (Desvios)
 # -----------------------------------------------------
+
+# Limpando area de plotagem
+dev.off()
+
+# Criando nomes 
 #kable(table(dp_menos5,dp_menos4,dp_menos3,dp_menos2,dp_menos1,media,dp_mais1,dp_mais2,dp_mais3,dp_mais4,dp_mais5))
 names1=c("-5DP", "-4DP", "-3DP",
          "-2DP", "-1DP", "media",
          "+1DP", "+2DP", "+3DP","+4DP","+5DP")
 
+# Gerando tabela 
 grid.table(c(dp_menos5,dp_menos4,dp_menos3,dp_menos2,dp_menos1,media,dp_mais1,dp_mais2,dp_mais3,dp_mais4,dp_mais5),names1)
 
 
-# -----------------------------------------------------
-# Resumo
-missing3dp=c()
+# -----------------------------------------------------------
+# Tabela 5 ponto da Mediana
+# -----------------------------------------------------------
+# Limpando area de plotagem
+dev.off()
+
+
+descritiva=summary(data_brasil$VALVALOR)
+mediana=as.numeric(descritiva[3]) # Mediana
+quartil_1=as.numeric(descritiva[2]) # 1 Quartil
+quartil_3=as.numeric(descritiva[5]) # 3 Quartil
+
+# LI
+LI=mediana-(1.5*(quartil_3-quartil_1));LI
+Ls=mediana+(1.5*(quartil_3-quartil_1));Ls
+
+# --------------------------------
+# Por resumo dos 5 pontos
+# --------------------------------
+
+
+data_brasil$VALVALOR=data_brasil$VALVALOR[is.na(data_brasil$VALVALOR)]=9999999999999
+# write.csv2(data_brasil,"dados3.csv")
+
+cinco_ponto_na=c()
+cinco_ponto=c() # variavel para armazenar valores
+for(i in 1:length(data_brasil$VALVALOR)){
+  if(data_brasil$VALVALOR[i]==9999999999999){
+    cinco_ponto_na[i]=9999999999999
+  }else
+    
+  # +ou- 3 dp
+  if(data_brasil$VALVALOR[i]<LI && data_brasil$VALVALOR[i]>Ls)
+    cinco_ponto[i]=1
+  else{
+    cinco_ponto[i]=0
+  }
+  #soma_cinco=sum(cinco_ponto,rm.na=T)
+}
+soma_cinco=sum(cinco_ponto,rm.na=T);soma_cinco
+
+
+
+
+# --------------------------------
+# Contagem de Missin :Verificar em serie que possui vazio
+# --------------------------------
+contagem_missing=c()
+
+
+
+for(i in 1:length(data_brasil$VALVALOR)){
+if(data_brasil$VALVALOR[i]==""){
+  contagem_missing[i]=1
+}
+}
+soma_missing=sum(contagem_missing)
+# ------------------
+#Dados totais
+# ------------------
+total=length(data_brasil$VALVALOR);total
+
+
+# -----------------------------------------------------------
+# 
+# -----------------------------------------------------------
+# Variaveis que receberao o calculo da quantidade de desvios
+qdt_3dp=c();qdt_4dp=c();qdt_5dp=c()
+
+# Construcao do algoritmo
 for(i in 1:length(data_brasil$VALVALOR)){
   # +ou- 3 dp
-  if(data_brasil$VALVALOR[1]<dp_menos3 && data_brasil$VALVALOR[1]>dp_menos3)
-    missing3dp[1]=1
+  if(data_brasil$VALVALOR[i]<dp_menos3 && data_brasil$VALVALOR[i]>dp_menos3)
+    qdt_3dp[i]=1
   else{
-    missing3dp[1]=0
+    qdt_3dp[i]=0
+  }
+  # +ou- 4 dp
+  if(data_brasil$VALVALOR[i]<dp_menos4 && data_brasil$VALVALOR[i]>dp_menos4)
+    qdt_4dp[i]=1
+  else{
+    qdt_4dp[i]=0
+  }
+  # +ou- 5 dp
+  if(data_brasil$VALVALOR[i]<dp_menos5 && data_brasil$VALVALOR[i]>dp_menos5)
+    qdt_5dp[i]=1
+  else{
+    qdt_5dp[i]=0
   }
 }
+som3qtd=sum(qdt_3dp);som4qtd=sum(qdt_4dp);som5qtd=sum(qdt_5dp)
 
-
+names2=c("+-3DP","+-4DP","+-5DP","Por 5 pts","Missing","Dados Total")
+grid.table(c(som3qtd,som4qtd,som5qtd,soma_cinco,soma_missing,total),names2)
 
 # -----------------------------------------------------
 # -----------------------------------------------------
@@ -91,10 +206,14 @@ data1=ipeadata('BPC_defi', type = "data.table")
 
 
 get_metadata('BPC_defi', type = "data.table")
-# -----------------------------------------------
 
-# -----------------------------------------------
+
+# -----------------------------------------------------
+# -----------------------------------------------------
 # Conteudo de pesquisa
+# -----------------------------------------------------
+# -----------------------------------------------------
+
 # https://cran.r-project.org/web/packages/ipeaData/ipeaData.pdf
 # -----------------------------------------
 # - Pacote para bordas em Tabelas
@@ -106,3 +225,4 @@ get_metadata('BPC_defi', type = "data.table")
 #              type = "data.table")
 # 
 # get_metadata(serie = NULL, type = "data.table")
+# rm(i,pacotes)
